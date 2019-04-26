d0661079535f6906d6c0d3a179b44620fe44f389
kok huai <kokhuai_liew@hotmail.com>
2019-04-26 11:07:50 +0800

    WIP

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    CSRF_ENABLED = True
+    CSRF_ENABLED = False
     SECRET_KEY = os.environ.get(
         'SECRET_KEY') or os.urandom(32)
 
