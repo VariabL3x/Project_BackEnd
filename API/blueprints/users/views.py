c33682e5e33de0c7bc07045d5706987ed0c66c59
kok huai <kokhuai_liew@hotmail.com>
2019-04-28 14:19:24 +0800

    created user model

--- API/blueprints/users/views.py
+++ API/blueprints/users/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint , request
+from models.user import User
+from flask.json import jsonify
+from werkzeug.security import generate_password_hash , check_password_hash
 
 users_api_blueprint = Blueprint('users_api',
                              __name__,
@@ -6,4 +9,31 @@ users_api_blueprint = Blueprint('users_api',
 
 @users_api_blueprint.route('/', methods=['GET'])
 def index():
-    return "USERS API"
+    selected_users = User.select()
+    result = jsonify({
+        'data' : [u.as_dict() for u in selected_users]
+    })
+    return result
+
+### CREATE NEW USER
+@users_api_blueprint.route('/', methods=['POST'])
+def create():
+    data = request.form
+    hashed_password = generate_password_hash(data['password'])
+
+    new_user = User(
+        email = data['email'],
+        username = data['username'],
+        password = hashed_password
+    )
+
+    new_user.save()
+
+    successfully_created = (User.get_or_none( User.username ==  new_user.username ) != None)
+
+    resutl = jsonify({
+        'status' : successfully_created,
+        'data': new_user.as_dict()
+    })
+
+    return result
